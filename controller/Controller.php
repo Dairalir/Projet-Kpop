c2946635a6a21f79d953f9a2614bcc21d8e0274b
Dairalir <daimanvm@gmail.com>
2022-10-11 11:02:08 +0200

    creation des fonctions

--- controller/Controller.php
+++ controller/Controller.php
@@ -12,3 +12,20 @@ function Bands()
     $bands = getBands();
     require 'vue/vueBands.php';
 }
+
+function Artists()
+{
+    $artists = getArtists();
+    require 'vue/vueArtists.php';
+}
+
+function Albums()
+{
+    $albums = getAlbums();
+    require 'vue/vueAlbums.php';
+}
+
+function Error404()
+{
+    require 'vue/vueError.php';
+}
